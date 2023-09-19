5138b340f4bf1b2e3af3d18307074d959f6712c6
Gregor Simcic <grega0simcic@gmail.com>
2023-09-19 20:17:34 +0200

    data changes

--- R/data.R
+++ R/data.R
@@ -1,6 +1,6 @@
 #'Wolf Monitoring Genetic Samples Data
 #'
-#'Metadata of selected genetic samples of wolves collected between XXXX and YYYY,
+#'Metadata of selected genetic samples of wolves collected between 2015 and 2021,
 #'in the scope of Slovenian National Wolf Monitoring
 #'
 #'@format
@@ -8,7 +8,7 @@
 #'\describe{
 #'   \item{Sample}{Sample unique identifier code}
 #'   \item{Date}{Date of sample collection (format: `YYYY-MM-DD`)}
-#'   \item{AnimalRef}{Identification string for patricular animal}
+#'   \item{AnimalRef}{Identification string for particular animal}
 #'   \item{GeneticSex}{Sex of animal to which the sample belong (format: `M` = male, `F` = female)}
 #'   \item{lat}{latitude (N-S) of the sample (CRS: WGS84; EPSG: 4326)}
 #'   \item{lng}{longitude (W-E) of the sample (CRS: WGS84; EPSG: 4326)}
@@ -19,23 +19,3 @@
 "wolf_samples"
 
 
-#'Wolf Monitoring Genetic Samples Data
-#'
-#'Metadata of selected genetic samples of wolves collected between XXXX and YYYY,
-#'in the scope of Slovenian National Wolf Monitoring
-#'
-#'@format
-#'A data frame with 407 rows and 8 columns:
-#'\describe{
-#'   \item{Sample}{Sample unique identifier code}
-#'   \item{Date}{Date of sample collection (format: `YYYY-MM-DD`)}
-#'   \item{AnimalRef}{Identification string for patricular animal}
-#'   \item{GeneticSex}{Sex of animal to which the sample belong (format: `M` = male, `F` = female)}
-#'   \item{IsAnimalReference}{Is a samle also a reference sample for particular animal (format: `1` = yes, `0` = no)}
-#'   \item{lat}{latitude (N-S) of the sample (CRS: WGS84; EPSG: 4326)}
-#'   \item{lng}{longitude (W-E) of the sample (CRS: WGS84; EPSG: 4326)}
-#'   \item{SType}{Type of the sample. (`Direct Saliva, Scat, Urine, Saliva, Tissue, Decomposing Tissue, Blood`) }
-#'}
-#'
-#'@source Slovenian National Wolf Monitoring
-"wolf_samples_old"
